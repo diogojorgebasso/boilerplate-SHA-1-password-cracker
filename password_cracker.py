e8044b68be2bfa0752656f7cf998c992d6e29faf
Diogo Basso <diogojbasso@gmail.com>
2021-03-20 02:55:47 +0000

    feat: first idea

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,19 @@
 import hashlib
 
-def crack_sha1_hash(hash):
-    
+def crack_sha1_hash(hash, use_salts=False):
+  with open('top-10000-passwords.txt') as fp:
+    passwords = fp.read()
+    if use_salts:
+      with open("known-salts.txt", 'r') as f:
+        salts = f.read()
+        for salt in salts:
+          for password in passwords:
+            salted = salt+password
+            if str(hashlib.sha1(salted.encode()).hexdigest()) == hash:
+              return password
+    else:
+      for senha in passwords:
+        if str(hashlib.sha1(senha.encode()).hexdigest()) == hash:
+          return senha
+      
+        
