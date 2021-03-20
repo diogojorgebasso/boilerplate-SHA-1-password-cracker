385059ee2965949139f8721ec2f9ebd57896d542
Diogo Basso <diogojbasso@gmail.com>
2021-03-20 03:25:15 +0000

    feat: heavily inspired by https://github.com/the-rectifier/boilerplate-SHA-1-password-cracker

--- password_cracker.py
+++ password_cracker.py
@@ -1,19 +1,25 @@
 import hashlib
 
 def crack_sha1_hash(hash, use_salts=False):
-  with open('top-10000-passwords.txt') as fp:
-    passwords = fp.read()
+    with open("top-10000-passwords.txt", 'r') as f:
+      passwords = [x.strip() for x in f.readlines()]
+
     if use_salts:
-      with open("known-salts.txt", 'r') as f:
-        salts = f.read()
+        with open("known-salts.txt", 'r') as saltList:
+          salts = [x.strip() for x in saltList.readlines()]
         for salt in salts:
-          for password in passwords:
-            salted = salt+password
-            if str(hashlib.sha1(salted.encode()).hexdigest()) == hash:
-              return password
-    else:
-      for senha in passwords:
-        if str(hashlib.sha1(senha.encode()).hexdigest()) == hash:
-          return senha
-      
+            for password in passwords:
+              salted = salt+password
+              if str(hashlib.sha1(salted.encode()).hexdigest()) == hash:
+                return password
+
+              salted = password+salt
+              if str(hashlib.sha1(salted.encode()).hexdigest()) == hash:
+                return password
+        return "PASSWORD NOT IN DATABASE"
         
+    else:
+        for password in passwords:
+            if str(hashlib.sha1(password.encode()).hexdigest()) == hash:
+                return password
+        return "PASSWORD NOT IN DATABASE"
